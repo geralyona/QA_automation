cd56da9c828936c2215e571bd5a7e1bd1544f632
Alyona A <geralyona@gmail.com>
2023-04-25 20:29:55 +0200

    sign in from config

--- tests/ui/test_sign_in_pom.py
+++ tests/ui/test_sign_in_pom.py
@@ -1,8 +1,9 @@
+from src.config.config import CONFIG
 
 def test_sing_in_pom(github_ui_app):
     login_page = github_ui_app.LoginPage
 
     login_page.go_to()
-    login_page.try_sign_in('username', 'password')
+    login_page.try_sign_in(CONFIG.get("USERNAME"), CONFIG.get("PASSWORD"))
 
     assert login_page.check_error_message()
